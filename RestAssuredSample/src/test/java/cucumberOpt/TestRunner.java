1c0cdcae969be510ebbf3cf90c8d9a10f2160b1c
Paramesh Vijay <parameshvijay@yandex.com>
2020-07-11 17:48:51 +0530

    Fast commit

--- RestAssuredSample/src/test/java/cucumberOpt/TestRunner.java
+++ RestAssuredSample/src/test/java/cucumberOpt/TestRunner.java
@@ -1,10 +1,5 @@
 package cucumberOpt;
 
-<<<<<<< HEAD
 //Sample TestRunner. Trying to add couple of more comments
-=======
-//Sample TestRunner. Trying to add couple of more comments in TestRunner.java
-//Checking STASH operations
->>>>>>> 5d8aadc... Added comments to check STASH
 public class TestRunner {
 }

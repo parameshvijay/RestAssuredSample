eb0d48816e0f0285fb7274fea34569be31e93224
Paramesh Vijay <parameshvijay@yandex.com>
2020-07-11 15:39:39 +0530

    Completed Stash operations

--- RestAssuredSample/src/test/java/cucumberOpt/TestRunner.java
+++ RestAssuredSample/src/test/java/cucumberOpt/TestRunner.java
@@ -1,6 +1,6 @@
 package cucumberOpt;
 
 //Sample TestRunner. Trying to add couple of more comments in TestRunner.java
-//Checking STASH operations
+//Checking STASH operations in TestRunner.java
 public class TestRunner {
 }
